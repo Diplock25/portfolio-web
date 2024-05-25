c8c9d4d2b7fa3e9204ca0b9c1b2cc45aa6586edc
burntpro <softwareprogramando@gmail.com>
2024-05-24 20:52:45 -0500

    modifying cheat code

---
+++ src/utilities/typing.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function() {
+  const typedText = document.querySelector(".typed-text");
+  const textToType = "Dante Vilchez";
+  let currentIndex = 0;
+
+  function typeText() {
+    if (currentIndex < textToType.length) {
+      typedText.textContent += textToType.charAt(currentIndex);
+      currentIndex++;
+      setTimeout(typeText, 90);
+    }
+  }
+
+  typeText();
+});
